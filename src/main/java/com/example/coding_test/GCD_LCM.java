aed4720375918384756a72a3071aed879870cd93
SiHyun-Lee113 <tlgus0113@gmail.com>
2022-12-16 15:57:02 +0900

    코딩테스트 연습 코드 in Intellij

---
+++ src/main/java/com/example/coding_test/GCD_LCM.java
@@ -0,0 +1,55 @@
+package com.example.coding_test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 최대공약수 GCD : Greatest Common Division
+ * 최소공배수 LCM : The Least Common Multiple
+ * */
+public class GCD_LCM {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        System.out.println(Arrays.toString(solution(n, m)));
+    }
+
+    private static int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int a;
+        int b;
+
+        a = Math.max(n, m);
+        b = Math.min(n, m);
+
+        int r = a%b;
+        int q = a/b;
+        if(r == 0) {
+            answer[0] = b;
+        }
+        else {
+            System.out.println(r);
+            while(r > 0) {
+                int r1 = a % b;
+                System.out.println(r1);
+                if(r1 == 0) {
+                    answer[0] = b;
+                    break;
+                } else {
+                    a = b;
+                    b = r1;
+                }
+            }
+        }
+        answer[1] = n*m / answer[0];
+
+        return answer;
+    }
+}
---
+++ src/main/java/com/example/coding_test/close_char.java
@@ -0,0 +1,37 @@
+package com.example.coding_test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class close_char {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(Arrays.toString(solution(s)));
+    }
+
+    private static int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char key = s.charAt(i);
+            if(map.containsKey(key)) {
+                answer[i] = i - map.get(key);
+                map.put(key, i);
+            } else {
+                answer[i] = -1;
+                map.put(key, i);
+            }
+        }
+
+
+        return answer;
+    }
+}
