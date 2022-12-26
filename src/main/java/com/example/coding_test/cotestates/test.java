d906fde8280b68577df5ad7313687e0014141544
SiHyun-Lee113 <tlgus0113@gmail.com>
2022-12-26 14:01:39 +0900

    디펜스 게임

---
+++ src/main/java/com/example/coding_test/cotestates/test.java
@@ -0,0 +1,11 @@
+package com.example.coding_test.cotestates;
+
+public class test {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append('a');
+
+    }
+}
---
+++ src/main/java/com/example/coding_test/defence_game.java
@@ -0,0 +1,36 @@
+package com.example.coding_test;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class defence_game {
+    public static void main(String[] args) {
+        int n = 7;
+        int k = 3;
+        int[] enemy = {4, 2, 4, 5, 3, 3, 1};
+
+        System.out.println(solution(n, k, enemy));
+    }
+
+    private static int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> defense = new PriorityQueue<>();
+        int i;
+        for (i = 0; i < enemy.length; i++) {
+            if (defense.isEmpty() || defense.size() < k) {
+                defense.add(enemy[i]);
+            }
+            else {
+                if (defense.peek() < enemy[i] && n - defense.peek() >= 0) {
+                    n -= defense.poll();
+                    defense.add(enemy[i]);
+                } else if (n - enemy[i] >= 0) {
+                    n -= enemy[i];
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return i;
+    }
+}
